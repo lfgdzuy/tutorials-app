0536bf7b23712c1aa83b470831d94d1700424afc
Leonardo Fagundez <lfgdzdev@gmail.com>
2022-02-09 16:09:38 -0300

    Add Tailwind CSS and FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
