249ee715f77155239e6be035a3179a30292fbe8c
Leonardo Fagundez <lfgdzdev@gmail.com>
2022-02-09 16:17:26 -0300

    Add New and Edit views

---
+++ src/views/tutorials/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <tutorial-form action="edit"></tutorial-form>
+  </div>
+</template>
+
+<script>
+import TutorialForm from "@/components/organisms/tutorial/Form.vue";
+export default {
+  name: "EditTutorial",
+  components: { TutorialForm },
+};
+</script>
+
+<style></style>
---
+++ src/views/tutorials/New.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <tutorial-form action="create" title="Nuevo Tutorial"></tutorial-form>
+  </div>
+</template>
+
+<script>
+import TutorialForm from "@/components/organisms/tutorial/Form.vue";
+export default {
+  name: "NewTutorial",
+  components: { TutorialForm },
+};
+</script>
+
+<style></style>
