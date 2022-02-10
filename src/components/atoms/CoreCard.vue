56ca38034b44593e0ce2dbf341a356d052b74429
Leonardo Fagundez <lfgdzdev@gmail.com>
2022-02-10 05:55:40 -0300

    Update CoreCard styles

--- src/components/atoms/CoreCard.vue
+++ src/components/atoms/CoreCard.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md">
     <header v-if="title">
-      <h2 class="text-left text-3xl font-bold pb-2 border-b-2 mb-2">
+      <h2
+        class="text-left text-3xl font-bold pb-2 border-b-2 mb-2 flex justify-between items-center"
+      >
         {{ title }}
+        <slot name="header-action"></slot>
       </h2>
-      <slot name="header-action"></slot>
     </header>
     <div class="mt-2">
       <p v-if="description" class="text-2xl font-bold text-gray-700">
