27e6416cb49a85707832fec32806c67d8de0e992
Leonardo Fagundez <lfgdzdev@gmail.com>
2022-02-10 06:33:06 -0300

    Update Index.vue

--- src/views/tutorials/Index.vue
+++ src/views/tutorials/Index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="flex flex-col items-center">
-    <div
-      class="w-full flex flex-col items-start"
-      v-if="searchTerm.trim() !== '' && tutorials.length > 0"
-    >
+    <div class="w-full flex flex-col items-start" v-if="showSearch">
       <div class="relative w-full">
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
           <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
@@ -21,22 +18,19 @@
           class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
           placeholder="Buscar"
           v-model="searchTerm"
-          @change="searchTutorials"
+          @keydown="
+            initialLoad = false;
+            searchTutorials();
+          "
         />
       </div>
-      <core-button
-        v-if="searchTerm !== ''"
-        class="mt-4"
-        @click="
-          searchTerm = '';
-          searchTutorials();
-        "
+      <core-button v-if="searchTerm !== ''" class="mt-4" @click="clearSearch"
         >Limpiar búsqueda</core-button
       >
     </div>
     <div class="w-full">
       <tutorials-empty-state
-        v-if="searchTerm === '' && tutorials.length === 0"
+        v-if="initialLoad && tutorials.length === 0"
       ></tutorials-empty-state>
       <tutorials-list
         v-else-if="tutorials.length > 0"
@@ -46,6 +40,7 @@
       <tutorials-zero-results
         v-else
         :search-term="searchTerm"
+        @clearSearch="clearSearch"
       ></tutorials-zero-results>
     </div>
   </div>
@@ -70,18 +65,30 @@ export default {
     return {
       searchTerm: "",
       tutorials: [],
+      initialLoad: true,
     };
   },
   methods: {
     searchTutorials() {
-      console.log("perform search! >>> ", this.searchTerm);
       tutorialsService.find(this.searchTerm).then((res) => {
         this.tutorials = res.data;
       });
     },
+    clearSearch() {
+      this.searchTerm = "";
+      this.searchTutorials();
+    },
   },
   mounted() {
     this.searchTutorials();
+  },
+  computed: {
+    showSearch() {
+      return (
+        (this.searchTerm.trim() === "" && this.tutorials.length > 0) ||
+        this.searchTerm.trim()
+      );
+    },
   },
 };
 </script>
