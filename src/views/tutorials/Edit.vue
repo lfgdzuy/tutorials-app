905a652647de76f196eda2b9b52f18d1349ef3a7
Leonardo Fagundez <lfgdzdev@gmail.com>
2022-02-11 04:26:00 -0300

    Update Edit.vue

--- src/views/tutorials/Edit.vue
+++ src/views/tutorials/Edit.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    <tutorial-form action="edit" title="Editar Tutorial"></tutorial-form>
+    <tutorial-form
+      v-if="tutorial !== null"
+      action="edit"
+      title="Editar Tutorial"
+      :tutorial="tutorial"
+    ></tutorial-form>
   </div>
 </template>
 
 <script>
 import TutorialForm from "@/components/organisms/tutorial/Form.vue";
+import tutorialsService from "@/services/tutorials";
+
 export default {
   name: "EditTutorial",
   components: { TutorialForm },
+  data() {
+    return {
+      tutorial: null,
+    };
+  },
+  created() {
+    this.getTutorial();
+  },
+  methods: {
+    getTutorial() {
+      const tutorialId = this.$route.params.id;
+      return tutorialsService.get(tutorialId).then((res) => {
+        this.tutorial = res.data;
+      });
+    },
+  },
 };
 </script>
 
